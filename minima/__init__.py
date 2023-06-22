5133e763b787dc023c1afaabf15ffb02b78bcb69
__mo_san__ <50895527+m0saan@users.noreply.github.com>
2023-06-22 14:54:26 +0100

    added the __repr__ for Module and update Sequential for better modules prints + Softmax:3

--- minima/__init__.py
+++ minima/__init__.py
@@ -3,3 +3,8 @@ from . import autograd
 from .autograd import Tensor, cpu, all_devices
 from . import operators
 from .operators import *
+
+from .ndarray import *
+from . import nn
+from . import init
+from . import optim
